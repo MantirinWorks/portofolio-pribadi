4430d4c43d9839bc37e40e4fceab37c612604f8f
Mantirin <mantirin.works@gmail.com>
2026-09-21 14:36:39 +0700

    tantangan js

--- Latihan/Latihan_H2_CSS/script.js
+++ Latihan/Latihan_H2_CSS/script.js
@@ -18,3 +18,21 @@ peran = "Senior Frontend Developer";
 jumlahProyek = 15;
 
 console.log(`Target saya beberapa tahun kedepan adalah menjadi ${peran} dengan portofolio sebanyak ${jumlahProyek} proyek.`);
+
+// Tantangan 1: Kalkulator Umur Dinamis
+const tahunLahir = 1998;
+const tahunSekarang = 2026;
+let umur = "tahunSekarang - tahunLahir";
+
+console.log(`Saya lahir pada tahun ${tahunLahir}, sehingga umur saya sekarang adalah ${umur} tahun.`);
+
+// Tantangan 2: Estimasi Sisa Target
+const totalHari = 30;
+const hariBerjalan = 8;
+let sisaHari = "totalHari - hariBerjalan";
+
+let estimasiProyek = Math.floor(sisaHari / 7);
+
+console.log(`Sudah ${hariBerjalan} hari berjalan dari program ${totalHari} hari menjadi Frontend Developer`);
+console.log(`Berarti, sekarang tinggal tersisa ${sisaHari} hari lagi untuk target bisa tercapai`);
+console.log(`Saya juga harus membuat proyek disetiap minggu nya, yang berarti masih ada ${estimasiProyek} proyek lagi yang harus dibuat`);
